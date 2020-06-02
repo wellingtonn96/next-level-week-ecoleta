591d0c4930423d5fb1885168f5d2061e86d7175a
Wellington Santos <weltossousa@gmail.com>
2020-06-02 17:17:59 -0300

    adicionando configuração com o banco de dados,

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
-import express from "express";
+import express, { json } from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get(`/users`, (request, response) => {
-  console.log("Listagem de usuários");
-
-  response.json(["Digo", "Cleiton", "Robson"]);
-});
+app.use(json());
+app.use(routes);
 
 app.listen(3333);
