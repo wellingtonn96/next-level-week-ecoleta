700b8a4838c37143b501fe9e8fb9b966989fd637
Wellington Santos <weltossousa@gmail.com>
2020-06-02 19:14:54 -0300

    adicionando listagem de items de coleta

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,12 +7,52 @@ router.get(`/items`, async (request, response) => {
 
   const serialazedItems = items.map((item) => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
 
   return response.json(serialazedItems);
+});
+
+router.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    longitude,
+    latitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertIds = await trx("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    longitude,
+    latitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+
+  await trx("point_items").insert(pointItems);
+
+  return response.json({ success: true });
 });
 
 export default router;
