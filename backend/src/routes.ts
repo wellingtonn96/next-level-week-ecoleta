6ce7c14e3e3cd09fe564670384489c73f08d6e70
Wellington Santos <weltossousa@gmail.com>
2020-06-02 21:41:43 -0300

    adicioando listagem de pontos por (filtro por estado/cidedeitmes)

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,58 +1,18 @@
 import { Router } from "express";
-import knex from "./database/connection";
+
+import PointsController from "./controllers/PointsController";
+import ItemsCOntroller from "./controllers/ItemsController";
+
+const pointsController = new PointsController();
+const itemsController = new ItemsCOntroller();
+
 const router = Router();
 
-router.get(`/items`, async (request, response) => {
-  const items = await knex("items").select("*");
+// index, show, create, update, delete
+router.get(`/items`, itemsController.index);
 
-  const serialazedItems = items.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-
-  return response.json(serialazedItems);
-});
-
-router.post("/points", async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    longitude,
-    latitude,
-    city,
-    uf,
-    items,
-  } = request.body;
-
-  const trx = await knex.transaction();
-
-  const insertIds = await trx("points").insert({
-    image: "image-fake",
-    name,
-    email,
-    whatsapp,
-    longitude,
-    latitude,
-    city,
-    uf,
-  });
-
-  const point_id = insertIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id,
-    };
-  });
-
-  await trx("point_items").insert(pointItems);
-
-  return response.json({ success: true });
-});
+router.post("/points", pointsController.create);
+router.get("/points/:id", pointsController.show);
+router.get("/points", pointsController.index);
 
 export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import express, { json } from "express";
 import path from "path";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 
-app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use(cors());
 app.use(json());
 app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
